3151311e41369a97a8cdd118165421c15a6196a6
marchdown <marchdown@gmail.com>
2011-04-29 14:43:25 +0400

    restored .emacs.d/init.el without hardcoded Dropbox path

--- .emacs.d/subdirs.el
+++ .emacs.d/subdirs.el
@@ -1,8 +1,12 @@
 ;; -*- no-byte-compile: t -*-
 ;; In load-path, after this directory should come
 ;; certain of its subdirectories.  Here we specify them.
+<<<<<<< HEAD
 (normal-top-level-add-to-load-path '(;"vendor"
 				     "emacs-packages" "doodles"))
+=======
+(normal-top-level-add-to-load-path '("vendor" "libs-dealing-with-python" "shared"))
+>>>>>>> 45b6d64... Adapting .emacs.d/init.el away from Dropbox
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
